c26bfb4305d53b25e45def106c1ebfb32d28066f
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2024-12-24 12:10:17 +0300

    Update solution.cpp

--- InvertBinaryTree/solution.cpp
+++ InvertBinaryTree/solution.cpp
@@ -1,25 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr) { return nullptr; } // Base case
+        if (root == nullptr) { return nullptr; } // base case
 
-        // Swap the left and right child
+        // swap the left and right child
         TreeNode* temp = root->left;
         root->left = root->right;
         root->right = temp;
 
-        // Recursively apply the same to child nodes
+        // recursively apply the same to child nodes
         invertTree(root->left);
         invertTree(root->right);
 
