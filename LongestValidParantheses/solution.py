8436cc0044cefb05230547c687c0157a25250ae1
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2026-09-14 17:26:07 +0300

    Update solution.py

--- LongestValidParantheses/solution.py
+++ LongestValidParantheses/solution.py
@@ -9,18 +9,18 @@ class Solution(object):
         allowed_chars = {'(', ')'}
         self.input_checker(s, allowed_chars)
 
-        stack = [-1]  # Initialize with -1 to handle base case of empty string
+        stack = [-1]  # init with -1 to handle base case
         max_len = 0
 
         for i, char in enumerate(s):
             if char == '(':
-                stack.append(i)  # Push the index of '('
+                stack.append(i)  # push the index of '('
             else:
-                stack.pop()  # Pop the last '(' index
+                stack.pop()  # pop the last '(' index
                 if stack:
-                    max_len = max(max_len, i - stack[-1])  # Calculate the length of valid substring
+                    max_len = max(max_len, i - stack[-1])  # calc the length of valid substr
                 else:
-                    stack.append(i)  # Reset the base index for the next valid substring
+                    stack.append(i)  # reset the base index for next valid substr
 
         return max_len
 
@@ -34,6 +34,6 @@ class Solution(object):
         
         for char in input_string:
             if char not in allowed_chars:
-                print("Invalid character detected: '{}'. Shutting down...".format(char))
-                sys.exit(1)  # Shut down w/ a non-zero exit code
+                print("Invalid character detected: '{}'.".format(char))
+                sys.exit(1)  # terminate w/ a non-zero exit code
         print("Input is valid.")
