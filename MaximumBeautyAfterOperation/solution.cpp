88646f5d367c02f462bbe9523af3a258f788e104
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2026-09-14 17:28:04 +0300

    Update solution.cpp

--- MaximumBeautyAfterOperation/solution.cpp
+++ MaximumBeautyAfterOperation/solution.cpp
@@ -13,23 +13,23 @@ public:
 
         for (int i = 1; i < n; i++) {
             if (nums[i] != firstElement) {
-                isAllSame = false; // If any element is different, return false
-                maxBeauty = 0; // Update maxBeauty
+                isAllSame = false; // if any elt is different
+                maxBeauty = 0; // update maxBeauty
             }
         }
         
         if (isAllSame != true) {
             sort(nums.begin(), nums.end());
             
-            // Use sliding window to count elements in the range [nums[i] - k, nums[i] + k]
+            // sliding window approach to count elts in range: [ nums[i] - k, nums[i] + k ]
             int left = 0;
             for (int right = 0; right < n; ++right) {
-                // While nums[right] - nums[left] > 2 * k, move the left pointer
+                // while [ nums[right] - nums[left] > 2 * k ], move the left ptr
                 while (nums[right] - nums[left] > 2 * k) {
                     left++;
                 }
-                // Update the maximum beauty
-                maxBeauty = max(maxBeauty, right - left + 1);
+                
+                maxBeauty = max(maxBeauty, right - left + 1); // update maxBeauty
             }
         }
 
